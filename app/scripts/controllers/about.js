bed3e8eff20a21ea0aaec4becdfe3b249e276b4c
SheriefBadran <sb222rf@student.lnu.se>
2015-02-24 13:40:00 +0100

    yo angular generated

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name activityApiClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the activityApiClientApp
+ */
+angular.module('activityApiClientApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name activityApiClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the activityApiClientApp
+ */
+angular.module('activityApiClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
